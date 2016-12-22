bec865b3e41e3ef705c2d5511d87a8370c322493
lescpsn <lescpsn@aliyun.com>
2016-12-22 18:19:24 +0800

    nodejs学习记录

---
+++ nodejs/es6/es6_006.js
@@ -0,0 +1,29 @@
+// es6_004.js
+
+//exports
+class Human { // 方式1: export直接导出成共有，供外部文件访问
+    constructor(name) { // 相当于构造函数
+        this.name = name;
+    }
+    getName() {
+        console.log("my name is:", this.name);
+    }
+}
+
+function runing() {
+    console.log("I can running");
+}
+
+
+function eating() {
+    console.log("I can eating");
+}
+
+// 方式2: export直接导出成共有，供外部文件访问
+// export {
+//     runing,
+//     eating
+// };
+
+runing();
+eating();
