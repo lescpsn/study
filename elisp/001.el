7105cf6106d0e4df8b8ebbb26787f06b9dcc8577
lescpsn <lescpsn@aliyun.com>
2017-01-05 10:43:50 +0800

    学习记录

---
+++ elisp/001.el
@@ -0,0 +1,17 @@
+(+ 1 2)
+(+ (+ 1 2) 3)
+(+ (+ (+ 1 2) 3) 4)
+
+
+(setq name "carhj")
+
+;;(insert "iota")
+;;(insert "Hello Every " name)
+(defun hello (name) (insert "Helloaa " name))
+(hello "aaa")
+
+
+
+
+
+
